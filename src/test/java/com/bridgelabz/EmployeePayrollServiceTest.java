b2d8367e751ad982c7d352edb1c97aa24fdc5ade
Kunal <kunalsonawane730@gmail.com>
2022-12-14 01:26:10 +0530

    Add: Employee Payroll Service program to store data in a perticular file

---
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    public void whenThreeEmployeeAddedShouldReturnCountOfEmployees() {
+        EmployeePayrollData[] employeeList = { new EmployeePayrollData(1, "Rama", 50000),
+                new EmployeePayrollData(2, "Shyama", 50000), new EmployeePayrollData(3, "Krishna", 50000) };
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(employeeList));
+        employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+        employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+        long entries = employeePayrollService.countEntries();
+        assertEquals(3, entries);
+    }
+}
